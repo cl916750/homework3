a1ae182a38bf468e331e93ed73a52999043dbd95
cl916750 <46973055+cl916750@users.noreply.github.com>
2021-05-01 17:46:55 -0400

    Update grsh.c

--- grsh.c
+++ grsh.c
@@ -1,14 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
-int main()
-{
-  char cmd[100];
-  do{
-    
-  }while(cmd != exit);
-  
+
+char *cmd = NULL;
+size_t cap = 0;
+
+int i;
+char *token;
+char *arr[256];
+
+void cd();
+void path();
+void end();
+void tokenize(char *cmd);
+
+int main(void){
+   while(1){
+      printf("grsh> ");
+      getline(&cmd, &cap, stdin);
+     
+      tokenize(cmd);
+      
+      if(strcmp(arr[0], "exit") == 0){
+         end();
+      } 
+   }   
+}
+
+void tokenize(char *cmd){
+   i = 0;
+   token = strtok(cmd, "\n");
+   while(token != NULL){
+      arr[i] = token;
+      i++;
+      token = strtok(NULL, "\n");
+   }
+   arr[i] = NULL;
+}
+
+void cd(){
+
+}
+
+void path(){
+
+}
+
+void end(){
+   exit(0);
 }
